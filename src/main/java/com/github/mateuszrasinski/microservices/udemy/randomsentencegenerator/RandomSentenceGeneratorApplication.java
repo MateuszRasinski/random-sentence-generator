baaeccd4f742469371130f151744da6d8724a8fb
Mateusz Rasiński <mateusz.rasinski@gmail.com>
2019-01-15 01:30:59 +0100

    Use Hystrix Dashboard

--- src/main/java/com/github/mateuszrasinski/microservices/udemy/randomsentencegenerator/RandomSentenceGeneratorApplication.java
+++ src/main/java/com/github/mateuszrasinski/microservices/udemy/randomsentencegenerator/RandomSentenceGeneratorApplication.java
@@ -3,11 +3,13 @@ package com.github.mateuszrasinski.microservices.udemy.randomsentencegenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableHystrix
+@EnableHystrixDashboard
 public class RandomSentenceGeneratorApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/github/mateuszrasinski/microservices/udemy/randomsentencegenerator/SentenceController.java
+++ src/main/java/com/github/mateuszrasinski/microservices/udemy/randomsentencegenerator/SentenceController.java
@@ -1,9 +1,11 @@
 package com.github.mateuszrasinski.microservices.udemy.randomsentencegenerator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/sentence")
+@RestController
+@RequestMapping("/sentence")
 class SentenceController {
 
     private final SentenceGenerator sentenceGenerator;
